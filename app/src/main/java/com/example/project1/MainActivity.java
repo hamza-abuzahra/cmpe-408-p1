d8b3e1ef573fadf66ce10c773cbaed246764d1de
hamza abuzahra <hka201210@hotmail.com>
2022-11-13 10:47:51 +0300

    code cleaned

--- app/src/main/java/com/example/project1/MainActivity.java
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,11 +16,11 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.Calendar;
 import java.util.Hashtable;
 
@@ -28,12 +31,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     // hashtable for city/code pairs
     Hashtable<String, String> cityList
             = new Hashtable<String, String>();
-    TextView pob;
+    TextView pob, resultTxt;
 
     EditText gpaInput, infoInput,
     idInput, firstNameInput, lastNameInput;
-    RadioGroup scholarshipRadio, genderRadio,
-    maleRadio, femaleRadio;
+    RadioGroup scholarshipRadio, genderRadio, maleRadio, femaleRadio;
     CheckBox infoCheck;
     private Button datePickerButton;
     Spinner facultySpinner, departmentSpinner, placeOfBirthSpinner;
@@ -54,7 +56,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             "English Language and Literature", "English Language Teacher Education", "History",
             "International Relations", "Music", "Psychology", "Sociology"}
     };
-    String[] departments = {"example", "test"};
+    String[] departments = {};
 
 
     @SuppressLint("MissingInflatedId")
@@ -63,40 +65,38 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // id input:
-        idInput = findViewById(R.id.inputStdId);
-        // first name input:
-        firstNameInput = findViewById(R.id.inputFirstName);
-        // last name input:
-        lastNameInput = findViewById(R.id.inputLastName);
+        // id input
+        idInput = findViewById(R.id.stdIdInput);
+
+        // name component
+        firstNameInput = findViewById(R.id.firstNameInput);
+        lastNameInput = findViewById(R.id.lastNameInput);
 
         // date picker component:
+        datePickerButton = findViewById(R.id.datePickerBtn);
         initializeDatePicker();
-        datePickerButton = findViewById(R.id.datePickerButton);
-        datePickerButton.setText(getTodayDate());
+        datePickerButton = findViewById(R.id.datePickerBtn);
 
         // place of birth picker spinner:
-        pob = findViewById(R.id.txtPob);
-        pob.setText("city of birth");
+        pob = findViewById(R.id.cityMsg);
         placeOfBirthSpinner = findViewById(R.id.placeOfBirthSpinner);
         pobAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, cityCodesList);
         initializeCityList();
         placeOfBirthSpinner.setAdapter(pobAdapter);
         placeOfBirthSpinner.setOnItemSelectedListener(this);
 
-
         // gender component
         genderRadio = findViewById(R.id.genderRadio);
 
         // faculty spinner
         facultySpinner = findViewById(R.id.facultySpinner);
-        fAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, facutlies);
+        fAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, facutlies);
         facultySpinner.setAdapter(fAdapter);
         facultySpinner.setOnItemSelectedListener(this);
 
         // department spinner
         departmentSpinner = findViewById(R.id.departmentSpinner);
-        dAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, departments);
+        dAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, departments);
         departmentSpinner.setAdapter(dAdapter);
 
         // gpa and scholarship initialization
@@ -106,25 +106,27 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         // additional info edit text
         infoCheck = findViewById(R.id.infoCheck);
         infoInput = findViewById(R.id.infoInput);
+
+        resultTxt = findViewById(R.id.resultTxt);
     }
 
+    // using date picker
+    // helper function for dates
+    private String dateToString(int day, int month, int year) {
+        return  day + "/"+ month + "/" + year;
+    }
 
-
-// PLACE OF BIRTH SECTION
-// using date picker
-
-
-// method to get today's date:
-    private String getTodayDate(){
+    // method to get today's date:
+    private int[] getTodayDate(){
         Calendar calendar  = Calendar.getInstance();
         int year = calendar.get(Calendar.YEAR);
         int month = calendar.get(Calendar.MONTH);
         month = month +1;
         int day = calendar.get(Calendar.DAY_OF_MONTH);
-        return dateToString(day, month, year);
+        return new int[]{day, month, year};
     }
 
-// method to initialize the date picker
+    // method to initialize the date picker
     public void initializeDatePicker(){
     DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
         @Override
@@ -137,7 +139,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             datePickerButton.setText(date);
         }
     };
-    // getting today's date
+        // getting today's date
+        int[] today = getTodayDate();
+        datePickerButton.setText(dateToString(today[0], today[1], today[2]));
         Calendar calendar  = Calendar.getInstance();
         int year = calendar.get(Calendar.YEAR);
         int month = calendar.get(Calendar.MONTH);
@@ -145,27 +149,19 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
          // creating a new date picker dialog
         datePickerDialog = new DatePickerDialog(this, AlertDialog.THEME_DEVICE_DEFAULT_DARK, dateSetListener
-                , year,month,day);
-        // limits the max date to the current date
-        datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
-
+                , today[2],today[1]-1,today[0]);
     }
-
-    private String dateToString(int day, int month, int year) {
-        return  day + " "+ month + " " + " " + year;
-    }
-
     public void openDateSelector(View view) {
         // show the initialized date picker
         datePickerDialog.show();
     }
 
+    // PLACE OF BIRTH SECTION
 
     // city code list:
-
-
     // place of birth city list picker list:
     public void initializeCityList(){
+        cityList.put("0", "City of Birth");
         cityList.put("212", "Istanbul Europe");
         cityList.put("216", "Istanbul Asia");
         cityList.put("312", "Ankara");
@@ -182,11 +178,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public void matchCodeToCity(int i){
         String citySelected = "";
         citySelected = cityList.get(cityCodesList[i]);
-        System.out.println("in function");
         pob.setText(citySelected);
     }
-
-
 
     // when check box is checked the edit text component will be active
     public void enableInput (View view){
@@ -200,13 +193,55 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     }
 
+    // submit button listener
+    public void submit (View view) {
+        // get all inputs
+        String id = idInput.getText().toString();
+        String fName = firstNameInput.getText().toString().trim();
+        String lName = lastNameInput.getText().toString().trim();
+        String bDate = datePickerButton.getText().toString();
+        String bCity = pob.getText().toString();
+        int genderRadioId = genderRadio.getCheckedRadioButtonId();
+
+        String faculty = facultySpinner.getSelectedItem().toString();
+        String department = departmentSpinner.getSelectedItem().toString();
+        String gpa = gpaInput.getText().toString();
+        int scholarshipRadioId = scholarshipRadio.getCheckedRadioButtonId();
+
+        String info = infoInput.getText().toString();
+
+        boolean flag = id.length() == 11 && fName.length() != 0 && lName.length() != 0 &&
+                !(Float.parseFloat(gpa) > 4) && genderRadioId != -1 && scholarshipRadioId != -1 &&
+                (!infoCheck.isChecked() || infoInput.getText().toString().trim().length() != 0) &&
+                facultySpinner.getSelectedItemPosition() != 0 && placeOfBirthSpinner.getSelectedItemPosition() != 0 &&
+                departmentSpinner.getSelectedItemPosition() != 0;
+        // check for conditions
+        if (!flag){
+            Toast.makeText(this, "input not valid, check again", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        RadioButton genderRadioBtn = findViewById(genderRadio.getCheckedRadioButtonId());
+        String gender = genderRadioBtn.getText().toString();
+
+        RadioButton scholarshipRadioBtn = findViewById(scholarshipRadio.getCheckedRadioButtonId());
+        String scholarship = scholarshipRadioBtn.getText().toString();
+
+        Spanned textmsg = Html.fromHtml("<b><i>ID :</i></b><font color=\"red\">" + id + "<br></font>" + "<b><i>Name :</i></b> <font color=\"red\">" + fName + "<br></font>" +
+                "<b><i>Last Name :</i></b> <font color=\"red\">" + lName + "<br></font>" + "<b><i>Birth Date :</i></b> <font color=\"red\">" + bDate + "<br></font>" + "<b><i>Birth City :</i></b> <font color=\"red\">" + bCity + "<br></font>" +
+                "<b><i>Gender :</i></b> <font color=\"red\">" + gender + "<br></font>" + "<b><i>Faculty :</i></b> <font color=\"red\">" + faculty + "<br></font>" + "<b><i>Department :</i></b> <font color=\"red\">" + department + "<br></font>" +
+                "<b><i>GPA :</i></b> <font color=\"red\">" + gpa + "<br></font>" + "<b><i>Scholarship :</i></b> <font color=\"red\">" + scholarship + "<br></font>" + "<b><i>Additional Info :</i></b> <font color=\"red\">" + info + "<br></font>");
+
+        resultTxt.setText(textmsg);
+    }
+
     // reset button - clears all fields
     public void reset (View view) {
-        // id
-        // names
-        // birth date
-        // birth place
-
+        idInput.setText("");
+        firstNameInput.setText("");
+        lastNameInput.setText("");
+        initializeDatePicker();
+        placeOfBirthSpinner.setSelection(0);
         genderRadio.clearCheck();
         facultySpinner.setSelection(0);
         gpaInput.setText("");
@@ -214,25 +249,30 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         infoCheck.setChecked(false);
         infoInput.setEnabled(false);
         infoInput.setText("");
+        resultTxt.setText("");
+    }
+
+    // click listener for exit button
+    public void exit (View view) {
+        finish();
+        System.exit(0);
     }
 
 
-    // spinner listener - faculty spinner
+    // spinner listener - faculty spinner & place of birth spinner
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        dAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, departmentList[i]);
-        departmentSpinner.setAdapter(dAdapter);
-        // calling function to match the city code to city name
-        matchCodeToCity(i);
-//        departments = departmentList[i];
-//        departmentSpinner.setClickable(true);
+        if (adapterView.getId() == facultySpinner.getId()){
+            dAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, departmentList[i]);
+            departmentSpinner.setAdapter(dAdapter);
+        }
+        if (adapterView.getId() == placeOfBirthSpinner.getId()){
+            matchCodeToCity(i);
+        }
     }
-
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
-
-
 }
